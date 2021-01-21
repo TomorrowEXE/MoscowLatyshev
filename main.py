b576e10f6400f0c858994f684134cf8d3f12e9f6
alexa <alexander05511@gmail.com>
2021-01-21 18:59:15 +0300

    Ui file yellow

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
+from PyQt5 import uic
+from random import randint
 import sys
-
-from PyQt5 import uic  # Импортируем uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('Ui.ui', self)
-        self.pushButton.clicked.connect(self.run)
+        self.pushButton.clicked.connect(self.circle)
+        canvas = QPixmap(391, 241)
+        self.label.setPixmap(canvas)
 
-    def run(self):
-        print('OK')
+    def circle(self):
+        x, y = [randint(10, 350) for _ in range(2)]
+        w, h = [randint(10, 100) for i in range(2)]
+        painter = QPainter(self.label.pixmap())
+        pen = QPen()
+        pen.setWidth(3)
+        pen.setColor(QColor(255, 255, 0))
+        painter.setPen(pen)
+        painter.drawEllipse(x, y, w, h)
+        painter.end()
+        self.update()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
